97da33520e694fb7b3a4998a344a31d2156823f8
Daniel Frankcom <daniel@frankcom.ca>
2022-12-03 13:36:18 -0800

    Add initial Vite site

---
+++ lib/web/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Who are you looking for?</h1>
+      <ul>
+        <li>Daniel</li>
+        <li>Diane</li>
+        <li>Paul</li>
+      </ul>
+    </div>
+  );
+}
